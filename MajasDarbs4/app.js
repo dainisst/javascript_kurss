9252e9b2c4e5221ee3d9b1d45a36d2a13a611035
dainist <dainis.st@gmail.com>
2023-08-01 11:41:02 +0300

    md

--- MajasDarbs4/app.js
+++ MajasDarbs4/app.js
@@ -15,7 +15,28 @@ function myFunction(){
 
 const registerBtn = document.querySelector("#registerBtn");
 const dialogCntn = document.querySelector("#dialogCntn");
+const confirmBtn = document.querySelector("#confirmBtn");
+const dialogCntn2 = document.querySelector("#dialogCntn2");
+const confirmBtn2 = document.querySelector("#confirmBtn2");
+const dialogCntn3 = document.querySelector("#dialogCntn3");
+
 
 registerBtn.addEventListener('click', () => {
   dialogCntn.showModal();
 })
+
+confirmBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  dialogCntn.close();
+  dialogCntn2.showModal();
+})
+
+confirmBtn2.addEventListener('click', (event2) => {
+  event2.preventDefault();
+  dialogCntn2.close();
+  dialogCntn3.showModal();
+  setTimeout(() => {
+    dialogCntn3.close();
+  }, "2000")
+})
+
