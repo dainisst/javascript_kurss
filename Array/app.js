37b76bd199e8c70f4ced094eb76f69a2f3144741
dainist <dainis.st@gmail.com>
2023-08-03 13:17:44 +0300

    array

--- Array/app.js
+++ Array/app.js
@@ -12,4 +12,4 @@ arr1.forEach((element) => container.innerHTML += `<p>${element}</p>`);
 const arr2 = ["jāuzraksta vēstle", "jānopērk velosipēds", "jāiztīra māja"];
 
 const myTable = document.querySelector(".myTable");
-arr2.forEach((item) => myTable.innerHTML += `<tr><td>${item}</td><td><button>X</button></td></tr>`);
+arr2.forEach((item) => myTable.innerHTML += `<tr id="tableRow"><td id="tableCol">${item}</td><td id=tableColBtn><button id="btn">X</button></td></tr>`);
