b2191274059840c8b652c146c486c0e713c992a1
dainist <dainis.st@gmail.com>
2023-08-08 13:45:27 +0300

    NE

--- Array/app.js
+++ Array/app.js
@@ -1,15 +1,23 @@
-const arr1 = ["suns", "kakis", "pele"];
-arr1.push("vilks", "lacis");
+// const arr1 = ["suns", "kakis", "pele"];
+// arr1.push("vilks", "lacis");
 
 
-arr1.forEach((element) => console.log(element));
+// arr1.forEach((element) => console.log(element));
 
 
-const container = document.querySelector(".container");
-arr1.forEach((element) => container.innerHTML += `<p>${element}</p>`);
+// const container = document.querySelector(".container");
+// arr1.forEach((element) => container.innerHTML += `<p>${element}</p>`);
 
 
-const arr2 = ["jāuzraksta vēstle", "jānopērk velosipēds", "jāiztīra māja"];
+const arr2 = [];
 
+const inputBtn = document.querySelector("#inputBtn");
 const myTable = document.querySelector(".myTable");
-arr2.forEach((item) => myTable.innerHTML += `<tr id="tableRow"><td id="tableCol">${item}</td><td id=tableColBtn><button id="btn">X</button></td></tr>`);
+
+inputBtn.addEventListener('click', () => {
+    const inputBox = document.querySelector("#inputBox").value;  
+    arr2.push(inputBox);     
+});
+
+arr2.forEach((item) => myTable.innerHTML += `<tr id="tableRow"><td id="tableCol">${item}</td><td id="tableColBtn"><button id="btn">X</button></td></tr>`);
+
